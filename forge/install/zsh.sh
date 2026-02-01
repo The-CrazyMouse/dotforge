444f6b7e1ea04c35481d67214f21b03602addcfd
The-CrazyMouse <andrerddantas@hotmail.com>
2026-02-01 20:39:39 +0000

    fix: fixed zsh instalation

--- forge/install/zsh.sh
+++ forge/install/zsh.sh
@@ -1,4 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source "$DOT_HOME/forge/lib/lib.sh"
+
+# Install Oh My Zsh (interactive is fine since you can exit)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mkdir -p $HOME/.config/oh-my-zsh/custom
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# Set custom path for Oh My Zsh
+export ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.config/oh-my-zsh/custom}"
+mkdir -p "$ZSH_CUSTOM/plugins"
+
+# Clone plugins if they don't already exist
+[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] && \
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+
+[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ] && \
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+
